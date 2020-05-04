950339f146e71d0ef046d66d4a78e43fa3bb3c7e
Steffen Sande <Steffen.Sande@bekk.no>
2020-05-04 11:46:39 +0200

    fix: 70 characters instead of 100 as default

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var options = {
     (process.env.CZ_MAX_HEADER_WIDTH &&
       parseInt(process.env.CZ_MAX_HEADER_WIDTH)) ||
     config.maxHeaderWidth ||
-    100,
+    70,
   maxLineWidth:
     (process.env.CZ_MAX_LINE_WIDTH &&
       parseInt(process.env.CZ_MAX_LINE_WIDTH)) ||
